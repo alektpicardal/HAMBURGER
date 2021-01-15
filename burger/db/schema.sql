33043fe362c5a6366c83e361472e3e3ebdc66645
alektpicardal <71862302+alektpicardal@users.noreply.github.com>
2021-01-14 18:17:15 -0800

    Add files via upload

---
+++ burger/db/schema.sql
@@ -0,0 +1,18 @@
+-- @author: Alek Picardal
+-- @github: alektpicardal
+-- @comment: Eat the Burger!
+
+
+
+-- Database Creation
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+
+-- Table Creation
+CREATE TABLE burgers(
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+burger_name VARCHAR(50),
+devoured BOOLEAN,
+date TIMESTAMP);
